055c1c9b2be2c63d3c8d1ebe55d98b3a086b551d
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-04-19 18:23:24 +0530

    Initial Peoject files

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = React.useState([]);
+
+  //This function is to add the newNote paased from the Create Area component into the array of Notes.
+  function addNote(newNote) {
+    setNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
+
+  //This function is to delete the item based on the id of the item from the array.
+  function deleteNote(id) {
+    setNotes((prevValue) => {
+      return prevValue.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
